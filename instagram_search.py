224f622e5c3fe745a7d92169bf29303b046b8be0
Tom Dickinson <tomkdickinson@gmail.com>
2016-12-15 10:23:11 +0000

    Adding in some comments, and changing the example query to something more festive and less depressing.

--- instagram_search.py
+++ instagram_search.py
@@ -8,6 +8,16 @@ from abc import ABCMeta, abstractmethod
 class InstagramUser:
 
     def __init__(self, user_id, username=None, bio=None, followers_count=None, following_count=None, is_private=False):
+        """
+        A class to represent an Instagram User
+
+        :param user_id: User ID of instagram user
+        :param username: Username of Instagram user
+        :param bio: Bio text for user
+        :param followers_count: Number of followers
+        :param following_count: Number of people following
+        :param is_private: Boolean to indicate if account is private or not
+        """
         self.id = user_id
         self.username = username
         self.bio = bio
@@ -19,6 +29,16 @@ class InstagramUser:
 class InstagramPost:
 
     def __init__(self, post_id, code, user=None, caption="", display_src=None, is_video=False, created_at=None):
+        """
+        A class to represent a post on Instagram
+        :param post_id: ID of the post
+        :param code: Code of the post
+        :param user: A user object representing the owner of the post
+        :param caption: The caption/text of the post
+        :param display_src: The URL of the image of the post
+        :param is_video: A boolean value indicating it's a video
+        :param created_at: The time it was created
+        """
         self.post_id = post_id
         self.code = code
         self.caption = caption
@@ -28,6 +48,10 @@ class InstagramPost:
         self.created_at = created_at
 
     def processed_text(self):
+        """
+        Processes a caption to remove newlines in it.
+        :return:
+        """
         if self.caption is None:
             return ""
         else:
@@ -35,6 +59,10 @@ class InstagramPost:
             return text
 
     def hashtags(self):
+        """
+        Simple hashtag extractor to return the hastags in the post
+        :return:
+        """
         hashtags = []
         if self.caption is None:
             return hashtags
@@ -46,12 +74,27 @@ class InstagramPost:
 
 class HashTagSearch(metaclass=ABCMeta):
 
-    def __init__(self, request_timeout=10000, error_timeout=10000, request_retries=3):
+    def __init__(self, request_timeout=10, error_timeout=10, request_retries=3):
+        """
+        This class performs a search on Instagrams hashtag search engine, and extracts posts for that given hashtag.
+
+        There are some limitations, as this does not extract all occurrences of the hash tag.
+
+        Instead, it extracts the most recent uses of the tag.
+
+        TODO - Perform an experiment to see "How Recent" most recent is
+
+        :param request_timeout: A timeout request
+        :param error_timeout: A timeout we sleep for it we experience an error before our next retry
+        :param request_retries: Number of retries on an error, before giving up
+        """
         super().__init__()
         self.request_timeout = request_timeout
         self.error_timeout = error_timeout
         self.request_retries = request_retries
         self.instagram_root = "https://www.instagram.com"
+
+        # We need a CSRF token, so we query Instagram first
         self.csrf_token, self.cookie_string = self.get_csrf_and_cookie_string()
         log.info("CSRF Token set to %s", self.csrf_token)
         log.info("Cookie String set to %s" % self.cookie_string)
@@ -59,8 +102,7 @@ class HashTagSearch(metaclass=ABCMeta):
     def extract_recent_tag(self, tag):
         """
         Extracts Instagram posts for a given hashtag
-        :param tag:
-        :return:
+        :param tag: Hashtag to extract
         """
 
         url_string = "https://www.instagram.com/explore/tags/%s/?__a=1" % tag
@@ -78,7 +120,7 @@ class HashTagSearch(metaclass=ABCMeta):
     def get_csrf_and_cookie_string(self):
         """
         This method connects to Instagram, and returns a list of headers we need in order to process further
-        requests
+        requests, including a CSRF Token
         :return: A header parameter list
         """
         resp = requests.head(self.instagram_root)
@@ -86,6 +128,12 @@ class HashTagSearch(metaclass=ABCMeta):
         return resp.cookies['csrftoken'], resp.headers['set-cookie']
 
     def get_next_results(self, tag, cursor):
+        """
+        Gets the next batch of results in the cursor.
+        :param tag: Hashtag to search
+        :param cursor: Cursor pagination object
+        :return: The next set of nodes and cursor
+        """
         log.info("Getting %s with cursor %s" % (tag, cursor))
         nodes = []
         next_cursor = cursor
@@ -100,9 +148,15 @@ class HashTagSearch(metaclass=ABCMeta):
                     next_cursor = request_json["media"]["page_info"]["end_cursor"]
         except Exception as e:
             log.error(e)
+
         return nodes, next_cursor
 
     def extract_instagram_posts(self, nodes):
+        """
+        For a given set of nodes from Instagrams JSON response, parse the nodes into Instagram Post objects
+        :param nodes: Instagram JSON nodes
+        :return: A list of Instagram objects
+        """
         posts = []
         for node in nodes:
             user = self.extract_owner_details(node["owner"])
@@ -118,6 +172,11 @@ class HashTagSearch(metaclass=ABCMeta):
 
     @staticmethod
     def extract_owner_details(owner):
+        """
+        Extracts the details of a user object.
+        :param owner: Instagrams JSON user object
+        :return: An Instagram User object
+        """
         username = None
         if "username" in owner:
             username = owner["username"]
@@ -128,6 +187,11 @@ class HashTagSearch(metaclass=ABCMeta):
         return user
 
     def get_headers(self, referrer):
+        """
+        Returns a bunch of headers we need to use when querying Instagram
+        :param referrer: The page referrer URL
+        :return: A dict of headers
+        """
         return {
             "referer": referrer,
             "accept": "application/json, text/javascript, */*; q=0.01",
@@ -145,10 +209,11 @@ class HashTagSearch(metaclass=ABCMeta):
     @staticmethod
     def get_query_param(tag, end_cursor):
         """
-        Returns the query params required to load next page on Instagram
-        :param tag:
-        :param end_cursor:
-        :return:
+        Returns the query params required to load next page on Instagram.
+        This can be modified to return less information.
+        :param tag: Tag we're querying
+        :param end_cursor: The end cursor Instagram specifies
+        :return: A dict of request parameters
         """
         return {
             'q':
@@ -194,8 +259,7 @@ class HashTagSearch(metaclass=ABCMeta):
     def save_results(self, instagram_results):
         """
         Implement yourself to work out what to do with each extract batch of posts
-        :param instagram_results:
-        :return:
+        :param instagram_results: A list of Instagram Posts
         """
 
 
@@ -214,4 +278,4 @@ class HashTagSearchExample(HashTagSearch):
 
 if __name__ == '__main__':
     log.basicConfig(level=log.INFO)
-    HashTagSearchExample().extract_recent_tag("brexit")
+    HashTagSearchExample().extract_recent_tag("christmas")
